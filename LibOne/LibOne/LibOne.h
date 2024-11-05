e602f3ce6c198bac2cacf3fa7abc6d9c446c535e
Steven Nance <stevenmnance@gmail.com>
2024-11-05 18:40:45 +0100

    scaffold LibOne

---
+++ LibOne/LibOne/LibOne.h
@@ -0,0 +1,18 @@
+//
+//  LibOne.h
+//  LibOne
+//
+//  Created by Steven Nance on 05/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibOne.
+FOUNDATION_EXPORT double LibOneVersionNumber;
+
+//! Project version string for LibOne.
+FOUNDATION_EXPORT const unsigned char LibOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibOne/PublicHeader.h>
+
+
